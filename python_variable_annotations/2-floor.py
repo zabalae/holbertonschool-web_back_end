e4af58fddd364a3fc5360dd1ec6a04cd64f2ddf6
Elizabeth <elizabeth.zabala3@gmail.com>
2024-04-23 02:16:32 -0400

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Type-annotated function that takes a float as an
+argument and returns the floor of the float'''
+
+
+def floor(n: float) -> int:
+    '''floor function'''
+    return int(n)
