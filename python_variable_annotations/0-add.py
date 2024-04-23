6d69d895a453a2471d6890f75a822f36ef361f78
Elizabeth <elizabeth.zabala3@gmail.com>
2024-04-23 02:08:05 -0400

    task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Type-annoted function that takes floats as
+arguments and returns their sum as a float'''
+
+
+def add(a: float, b: float) -> float:
+    '''add function'''
+    return a + b
