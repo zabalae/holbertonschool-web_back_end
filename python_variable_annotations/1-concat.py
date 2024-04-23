2079ca14620cc1eaed4f23cc952b9203ae0aeb52
Elizabeth <elizabeth.zabala3@gmail.com>
2024-04-23 02:13:12 -0400

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Type-annotated function that takes strings as
+arguments and returns a concatenated string'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''concat function'''
+    return str1 + str2
