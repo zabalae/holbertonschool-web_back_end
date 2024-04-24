f1dc08838beba41809e4ca6a804c950afeecbd4e
Elizabeth <elizabeth.zabala3@gmail.com>
2024-04-24 00:20:07 -0400

    task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Type-annoted function which takes a list of floats
+as argument and returns their sum as a float'''
+
+
+def sum_list(input_list: list[float]) -> float:
+    '''sum_list function'''
+    return sum(input_list)
