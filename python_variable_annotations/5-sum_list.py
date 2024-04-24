4348b444ef3f3b156700bd1313e2d21f42f91271
Elizabeth <elizabeth.zabala3@gmail.com>
2024-04-24 01:12:10 -0400

    task 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,6 +3,7 @@
 as argument and returns their sum as a float'''
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     '''sum_list function'''
     return sum(input_list)
