025ec416c2663e2c1478ce6f674b7a41987f3dab
Elizabeth <elizabeth.zabala3@gmail.com>
2024-04-23 23:56:45 -0400

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Type-annotated function that takes a float n as an
+argument and returns the string representation of the float'''
+
+
+def to_str(n: float) -> str:
+    '''to_str function'''
+    return str(n)
