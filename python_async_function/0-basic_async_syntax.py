c7ecd07eb41f84125eb3913095db5b1da408ddcf
Elizabeth <elizabeth.zabala3@gmail.com>
2024-04-24 22:30:04 -0400

    task 0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''coroutine that takes in an integer argument that waits
+for a random delay between 0 and max_delay'''
+import random, asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    '''Wait random'''
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+
+    return delay
