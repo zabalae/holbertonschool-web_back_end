e4ec9a41592fca26b9085e03430981d798619c8a
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-04-27 01:49:23 -0400

    Task 2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -13,6 +13,6 @@ async def measure_runtime() -> float:
     '''Measure the runtime of async_comprehension'''
 
     start = time.time()
-    await asyncio.gather(*async_comprehension() for x in range(4))
+    await asyncio.gather(*(async_comprehension() for x in range(4)))
     end = time.time()
     return end - start
