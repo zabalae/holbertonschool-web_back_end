00846fc48ec7fbc7d3d2f5c6ca6cdac4d35e1b40
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-04-26 23:47:56 -0400

    Task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''The async_generator coroutine will loop 10 times then
+yield a random number between 0 and 10'''
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[int, None, None]:
+    '''The async_generator coroutine will yield a random
+    number between 0 and 10'''
+
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
