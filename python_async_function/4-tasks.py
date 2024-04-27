770afefb820dc9522c4e10aee681d382386fdf6c
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-04-26 23:30:38 -0400

    Task 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -9,7 +9,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''task_wait_n function'''
     delays = []
     tasks = []
