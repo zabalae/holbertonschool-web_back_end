8a1220d944acae072b04122267e9c4a674904039
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-04-26 23:20:03 -0400

    Task 4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''Take the code from wait_n and alter it into a new function
+task_wait_n. The code is nearly identical to wait_n except
+task_wait_random is being called.'''
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay) -> List[float]:
+    '''task_wait_n function'''
+    delays: List[float] = []
+    tasks = []
+
+    for x in range(n):
+        delays.append(task_wait_random(max_delay))
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    return delays
