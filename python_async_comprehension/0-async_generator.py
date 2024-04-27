391609d23ac7a4d1d74df7260d536752c3d9f471
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-04-26 23:50:16 -0400

    Task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     '''The async_generator coroutine will yield a random
     number between 0 and 10'''
 
