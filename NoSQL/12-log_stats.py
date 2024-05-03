7fdb7668fd192ec7ae49b92fd1e8147ca09a9c84
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-03 06:05:17 -0400

    Task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     '''logs stats about Nginx logs'''
-    client = MongoClient('mongodb://localhost:27017/')
+    client = MongoClient('mongodb://127.0.0.1:27017/')
     collection = client.logs.nginx
 
     print(f'{collection.count_documents({})} logs')
