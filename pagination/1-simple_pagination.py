f7368979b9f2043ece73536b9e839b0627bcd8bc
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-03 15:10:15 -0400

    Task 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -33,7 +33,7 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         '''get_page function'''
         assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
+        assert isinstance(page_size, int) and page > 0
 
         init, end = index_range(page, page_size)
         return self.__dataset[init:end]
