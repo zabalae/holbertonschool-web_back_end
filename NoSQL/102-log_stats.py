f03f0cf78aa2559c9555cd3a57a7a89cd9128cdc
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-03 06:25:51 -0400

    Task 15

--- NoSQL/102-log_stats.py
+++ NoSQL/102-log_stats.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
         {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
         {"$sort": {"count": -1}},
         {"$limit": 10},
-        {"$project": {"_id": 0, "$ip": "$_id", "count": 1}},
+        {"$project": {"_id": 0, "ip": "$_id", "count": 1}},
     ])
     print("IPs:")
     for top_ip in ips:
