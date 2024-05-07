cc818bd13032f5cc196890e8907bd350dde1b3c0
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-07 17:01:03 -0400

    Task 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,8 +1,10 @@
 export default function taskBlock(trueOrFalse) {
+  // Initialize task variables with default values
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
+    // Update task variables based on the input parameter
     const task = true;
     const task2 = false;
   }
