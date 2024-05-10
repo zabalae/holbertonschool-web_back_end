06eae2d3ef8c7432a6864334f5d4caff44bd3df8
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-09 23:00:38 -0400

    Task 14

--- ES6_basic/101-iterateThroughObject.js
+++ ES6_basic/101-iterateThroughObject.js
@@ -1,8 +1,8 @@
- export default function iterateThroughObject(reportWithIterator) {
+export default function iterateThroughObject(reportWithIterator) {
   const arr = [];
   for (const x of reportWithIterator) {
     arr.push(x);
   }
 
   return arr.join(' | ');
- }
+}
