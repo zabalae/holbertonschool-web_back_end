ff898b7ff0a5fecbd8d78264c789167e573ad811
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-10 18:54:34 -0400

    Task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from './3-currency';
+
+class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    if (typeof amount === 'number') this._amount = amount;
+  }
+
+  set currency(currency) {
+    if (currency instanceof Currency) this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.name})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
