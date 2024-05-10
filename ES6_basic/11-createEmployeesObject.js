77eb592a05e2195c66814db5f0a322bcab72248b
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-09 22:01:59 -0400

    Task 11

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,4 @@
 export default function createEmployeesObject(departmentName, employees) {
-  return {
-          [departmentName]: [...employees],
+  return {[departmentName]: [...employees],
   };
 }
