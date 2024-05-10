38e1f749b2ea88734f899610cc9607ca0f2ccb24
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-09 21:31:05 -0400

    Task 8

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -9,6 +9,6 @@ export default function getBudgetForCurrentYear(income, gdp, capita) {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
     [`capita-${getCurrentYear()}`]: capita,
-};
+  };
   return budget;
 }
