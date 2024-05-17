5ff94e7e9014ea917666f9caa004ddbabfa974e1
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-05-16 23:13:21 -0400

    Update 2-then.js

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,9 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-      .then(() => ({
+    .then(() => ({
       status: 200,
       body: 'successs',
-      }))
-      .catch(() => Error())
-      .finally(() => console.warn('Got a response from the API'));
+    }))
+    .catch(() => Error())
+    .finally(() => console.warn('Got a response from the API'));
 }
