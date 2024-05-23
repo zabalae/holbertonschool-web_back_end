646bd5339fa0ee44a75d5a44586a2fbe7f4ce21d
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-05-22 22:28:18 -0400

    Task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  if (startString === undefined) return '';
+  if (startString.length === 0) return '';
+
+  return[...set]
+    .filter((str) => (str !== undefined ? str.startsWith(startString) : ''))
+    .map((str) => (str !== undefined ? str.slice(startString.length) : ''))
+    .join('-');
+}
