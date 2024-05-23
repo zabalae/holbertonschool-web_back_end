8cadf1b69226ed3260d059bafb68a1f904224ab1
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-05-22 20:24:32 -0400

    Task 4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,14 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const noGrade = { grade: 'N/A' }
+  if (students instanceof Array) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (newGrades.filter((grade) => grade.studentId === student.id).pop() || noGrade).grade,
+      }));
+  }
+  return [];
+}
