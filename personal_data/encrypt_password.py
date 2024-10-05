6b6b86ead553ee4408d3564081070ec8b2ad4f9a
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-10-04 23:54:49 -0400

    task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -7,3 +7,14 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ hash encryption """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validate that the provided password matches the hashed password.
+    """
+    if bcrypt.checkpw(password.encode(), hashed_password):
+        return True
+    else:
+        return False
+    
