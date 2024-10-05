71c02995ea71f83f7204533db84e3cc9c5dc58c1
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-10-05 11:54:18 -0400

    task

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -15,9 +15,11 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv('AUTH_TYPE')
 
+
 if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
+
 
 if AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
