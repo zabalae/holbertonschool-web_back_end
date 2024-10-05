d567cae84abca6ff2fa5ea245bf9abc67cf94e92
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-10-05 14:06:25 -0400

    task

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -13,17 +13,28 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-AUTH_TYPE = getenv('AUTH_TYPE')
+AUTH_TYPE = os.getenv('AUTH_TYPE')
 
 
 if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
-
-if AUTH_TYPE == 'basic_auth':
+elif AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+
+elif AUTH_TYPE == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+
+elif AUTH_TYPE == 'session_exp_auth':
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+
+elif AUTH_TYPE == 'session_db_auth':
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
