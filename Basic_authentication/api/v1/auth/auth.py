e2fd3aefeec584b0d711c821ca33d6e6df7de5d1
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-10-05 11:57:33 -0400

    task

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -7,7 +7,7 @@ from typing import List, TypeVar
 class Auth():
     """Class that manages api authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Returns False
+        """Returns False
         """
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
@@ -35,7 +35,8 @@ class Auth():
 
 
     def authorization_header(self, request=None) -> str:
-        """header check"""
+        """header check
+        """
         if request is None:
             return None
 
@@ -43,6 +44,7 @@ class Auth():
 
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """current user"""
+        """current user
+        """
         return None
  
