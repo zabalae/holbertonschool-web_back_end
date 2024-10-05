a163f4153682af929d16532275589d534534dce0
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-10-05 14:19:35 -0400

    task

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -70,11 +70,14 @@ def before_request() -> str:
     if auth is not None and \
             auth.require_auth(request.path, exclude_paths) is True:
 
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None \
+                and auth.session_cookie(request) is None:
             abort(401)
 
         if auth.current_user(request) is None:
             abort(403)
+
+        request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
