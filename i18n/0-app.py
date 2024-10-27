dae331e4a78bcb5fbfb5ba220dd572cde9362349
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-10-26 21:02:23 -0400

    task 0

---
+++ i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic flask app"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Return a simple homepage"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
