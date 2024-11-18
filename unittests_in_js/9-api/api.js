704f57a0c3941f179f0f04234d0470471049700b
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-11-17 21:41:51 -0400

    task 9

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -4,11 +4,13 @@ const app = express();
 const port = 7865;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
-})
+  res.end('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.end(`Payment methods for cart ${req.params.id}`);
+});
 
 app.listen(port, () => {
-    console.log(`API available on localhost port ${port}`);
-  })
-
-module.exports = app;
+  console.log('API available on localhost port 7865');
+});
