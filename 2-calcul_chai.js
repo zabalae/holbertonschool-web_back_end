1a3e48b45f5b559eeacc02ffdeee7515270f3db3
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-11-17 20:26:38 -0400

    task 2

---
+++ 2-calcul_chai.js
@@ -0,0 +1,19 @@
+const calculateNumber = (type, a, b) => {
+    const numA = Math.round(a);
+    const numB = Math.round(b);
+  
+    if (type === 'SUBTRACT') {
+      return numA - numB;
+    }
+  
+    if (type === 'DIVIDE') {
+      if (bRound === 0) {
+        return 'Error';
+      }
+      return numA / numB;
+    }
+  
+    return numA + numB;
+  };
+  
+  module.exports = calculateNumber;
