d10b89a6965bc8d6ebbe09e81ea3772a99ee1bd5
Elizabeth Mendez <40727857+zabalae@users.noreply.github.com>
2024-11-17 21:35:32 -0400

    task 8

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,14 +1,19 @@
 const request = require('request');
-const chai = require('chai')
 const { expect } = require('chai');
 
-describe('API test', () => {
-      it('respond with 200 code and body', (done) => {
-        request('http://localhost:7865', 'GET', (err, res, body) => {
-          if (err) throw err;
-          expect(res.statusCode).to.equal(200);
-          expect(body).to.equal('Welcome to the payment system');
-        });
+describe('Integration Testing', () => {
+  describe('GET /', () => {
+    it('Code: 200 | Body: Welcome to the payment system', (done) => {
+      const options = {
+        url: 'http://localhost:7865',
+        method: 'GET',
+      };
+
+      request(options, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
         done();
+      });
     });
+  });
 });
